8290593c84fa2ffa0bb507fa5aefe957e9bce10a
Adrian Bardossy <bardossy.adrian@gmail.com>
2025-10-20 10:27:52 +0200

    Updating config

--- config.tf
+++ config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-adrian-bardossy"
+    bucket         = "terraform-interview-state-bucket"
     key            = "terraform/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-lock-state-file"
