5ece8ddc992abf489b2f5f3231d1b49c250fe33f
Adrian Bardossy <bardossy.adrian@gmail.com>
2025-10-20 11:20:44 +0200

    Updating config

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "private_key_name" {
   value = module.KP.ec2_private_key_name
 }
 
+# Security groups
 output "my_ip_ssh_name" {
   value = module.Security_Group.my_ip_ssh_name
 }
@@ -14,6 +15,32 @@ output "my_ip_ssh_arn" {
 output "my_ip_ssh_id" {
   value = module.Security_Group.my_ip_ssh_id
 }
+
+output "Alb_SG_name" {
+  value = module.Security_Group.Alb_SG_name
+}
+
+output "Alb_SG_arn" {
+  value = module.Security_Group.Alb_SG_arn
+}
+
+output "Alb_SG_id" {
+  value = module.Security_Group.Alb_SG_id
+}
+
+output "Nginx_SG_name" {
+  value = module.Security_Group.Nginx_SG_name
+}
+
+output "Nginx_SG_arn" {
+  value = module.Security_Group.Nginx_SG_arn
+}
+
+output "Nginx_SG_id" {
+  value = module.Security_Group.Nginx_SG_id
+}
+
+
 
 output "public_ip" {
   value = module.EC2-instance.ubuntu_eip_public_ip
