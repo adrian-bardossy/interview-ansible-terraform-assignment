1c5258f9bf5766d4cd6e26dbfd3a4c3ed468e233
Adrian Bardossy <bardossy.adrian@gmail.com>
2025-10-20 10:49:07 +0200

    Updating config

--- modules/VPC/vpc.tf
+++ modules/VPC/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">=5.19"
+  version = "5.7.1"
 
   name                 = var.vpc_name
   cidr                 = local.vpc_subnet
